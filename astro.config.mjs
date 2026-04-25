eb8d1bc1820c1b75ac665b903fdaa28429648d68
Hector Rodriguez <hector@helixcloud.dev>
2026-04-25 11:42:03 -0500

    add the cname file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hrod.dev',
   vite: {
     plugins: [tailwindcss()]
   }
